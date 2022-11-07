eef61b9d74b9a1277d645466abdf821a1d14ddb8
Kennan Hunter <kennanhunter5@gmail.com>
2022-11-06 22:36:43 -0500

    create workers server and type package

--- packages/datalib/src/index.ts
+++ packages/datalib/src/index.ts
@@ -1,16 +1,19 @@
+import type { Data } from "types";
+
 let scrolled = false;
 
-export const data = () => ({
-	browser: navigator.userAgent,
-	language: navigator.language,
-	location: window.location.href,
-	referrer: document.referrer,
-	timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-	size: {
-		width: screen.width,
-		height: screen.width,
-	},
-});
+export const data = () =>
+	({
+		browser: navigator.userAgent,
+		language: navigator.language,
+		location: window.location.href,
+		referrer: document.referrer,
+		timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+		size: {
+			width: screen.width,
+			height: screen.width,
+		},
+	} as Data);
 
 export const onScroll = () => {
 	// TODO:
---
+++ packages/types/src/index.ts
@@ -0,0 +1,11 @@
+export interface Data {
+	browser: string;
+	language: string;
+	location: string;
+	referrer: string;
+	timezone: string;
+	size: {
+		width: number;
+		height: number;
+	};
+}
